aebc6d63f6ae7839f4c6306f34996a4cc4016482
Varsha Sahu <varshasahu64043@gmail.com>
2025-06-22 16:47:51 +0530

    Removed Hardcoded Secrets

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA6b5nLwInDUS0ZqqoJ7A5UY4XL3gBOw2k",
-    authDomain: "app-8ecbc.firebaseapp.com",
-    projectId: "app-8ecbc",
-    storageBucket: "app-8ecbc.appspot.com", // ✅ FIXED
-    messagingSenderId: "1018175176922",
-    appId: "1:1018175176922:web:94abb084b7ab2311f865e3",
-    databaseURL: "https://app-8ecbc-default-rtdb.firebaseio.com",
-    measurementId: "G-L8WR38T89T"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    databaseURL: process.env.DATABASEURL,
+    measurementId: process.env.MEASUREMENTID
 };
 
 export const app = initializeApp(firebaseConfig);
